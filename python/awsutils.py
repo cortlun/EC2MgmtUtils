91450d2584f858ea5644308ed9fdf8f5af5b5219
Cort Lunke <cortlun@gmail.com>
2016-05-04 08:29:11 -0500

    Fixed some things

--- python/awsutils.py
+++ python/awsutils.py
@@ -2,6 +2,7 @@ from subprocess import call
 import os
 from os.path import expanduser
 import urllib.request
+from Crypto.PublicKey import RSA
 
 class ChildProcessUtils:
     def __init__(self, log_file):
@@ -28,14 +29,19 @@ class AwsUtils:
         self.cp_utils.spawn_child_process(["aws", "iam", "create-access-key", "--user-name", user_name])
     def create_group(self, group_name):
         self.cp_utils.spawn_child_process(["aws", "iam", "create-group", "--group-name", group_name])
-    def create_key_pair(self,key, key_fingerprint, key_name):
-        self.cp_utils.spawn_child_process(["aws", "ec2", "create-key-pair", "--key-name", "key_name", "--key-material", key, "--key-fingerprint", key_fingerprint])
+    def create_key_pair(self, key, key_fingerprint, key_name):
+        self.cp_utils.spawn_child_process(["aws", "ec2", "create-key-pair", "--key-name", key_name, "--key-material", key, "--key-fingerprint", key_fingerprint])
     def create_policy(self,policy_name, policy_document):
         self.cp_utils.spawn_child_process(["aws", "iam", "create-policy", "--policy-name", policy_name, "--policy-document", policy_document])
     def create_security_group(self,security_group, description):
         self.cp_utils.spawn_child_process(["aws", "ec2", "create-security-group", "--group-name", security_group, "--description", description])
     def create_user(self,user_name):
         self.cp_utils.spawn_child_process(["aws", "iam", "create-user", "--user-name", user_name])
+    def import_key_pair(self, public_key_location, key_name):
+        with open(public_key_location) as public_key:
+            keydata = public_key.read()
+        pubkey = RSA.importKey(binPubKey)
+        print(pubkey)
     def run_instances(self,image_id, count, instance_type, key_name, security_group):
         self.cp_utils.spawn_child_process(["aws", "ec2", "run-instances", "--image-id", image_id, "--count", count, "--instance-type", instance_type, "--key-name", key_name, "--security-group", security_group])
     def revoke_firewall_privelege(self,group_name, port, protocol, ip):
@@ -47,15 +53,15 @@ class AwsUtils:
         self.cp_utils.spawn_child_process(["aws", "ec2", "authorize-security-group-ingress", "--group-name", group_name, "--protocol", protocol, "--port", port, "--cidr", ip])
     def authorize_firewall_privelege(self,group_name, port, protocol):
         previous_ip = get_previous_ip()    
-        ip = get_current_ip()
+        ip = get_my_current_ip()
         if previous_ip is not None:
             revoke_firewall_privelege(group_name, port, protocol, previous_ip)        
         self.cp_utils.spawn_child_process(["aws", "ec2", "authorize-security-group-ingress", "--group-name", group_name, "--protocol", protocol, "--port", port, "--cidr", ip])
-    def get_current_ip(self):
+    def get_my_current_ip(self):
         return urllib.request.urlopen("http://checkip.amazonaws.com").read().decode("utf-8").rstrip("\r\n") + "/32"
     def log_current_ip(self):
         with open(previous_ip_location, "w+") as f:
-            f.write(get_current_ip())
+            f.write(get_my_current_ip())
     def get_previous_ip(self):
         try:
             lines = list(open(previous_ip_location))
@@ -64,7 +70,7 @@ class AwsUtils:
             return None
 
 if __name__ == "__main__":
-    print("Current ip: " + get_current_ip())
+    print("Current ip: " + get_my_current_ip())
     print("Logging current ip." )
     log_current_ip()
     print("Previous ip: " + get_previous_ip())
--- python/init-group-project.py
+++ python/init-group-project.py
@@ -1,7 +1,12 @@
 import awsutils
 	
+#A group project object necessary for initializing a group project.
+#It currently assumes a single EC2 instance per project.
 class GroupProject:
-	def __init__(self, project_name, members, class_id, admin_user):
+	#Constructor variables are necessary for creating group project objects in AWS
+	#Most objects (eg security group, group, instance name, policies, etc.) are derived
+	#from project name
+	def __init__(self, project_name, members, class_id, admin_user, pub_key):
 		self.admin_user = admin_user
 		self.members = members
 		self.project_name = project_name.upper()
@@ -10,14 +15,20 @@ class GroupProject:
 		self.security_group_name = self.project_name + "_SECURITY_GROUP"
 		self.class_id = class_id.upper()
 	def run(self):
-		self.create_group_and_members()
+		#self.create_group_and_members()
+		self.import_key_pair()
 	def create_group_and_members(self):
 		self.aws_utils.create_group(self.group_name)
 		for member in self.members:
 			self.aws_utils.create_user(member.id)
 			self.aws_utils.create_access_key(member.id)
 			self.aws_utils.add_user_to_group(member.id, self.group_name)
-	def launch_instance(self):
+	def import_key_pair(self):
+		self.aws_utils.import_key_pair(pub_key, project_name + "_KEY_PAIR")
+	#def create_security_group(self):
+		
+	#def launch_instance(self):
+		#self.aws_utils.run_instances()
 		
 class Member:
 	def __init__(self, first_name, last_name, email):
@@ -25,11 +36,16 @@ class Member:
 		self.last_name = last_name
 		self.email = email
 		self.id = self.first_name[:1].lower() + self.last_name.lower()
-		
+
+#Simple main method for initializing a group project
 if __name__ == "__main__":
 	members = []
 	#Initialize group members
 	members.append(Member("Jim", "Boe", "jboe@stthomas.edu"))
 	members.append(Member("John", "Doe", "jdoe@stthomas.edu"))
-	group_project = GroupProject("PYTHON_TEST_PROJECT", members, "PYTHON_TEST_CLASS", "clunke")
+	
+	#Create group project
+	group_project = GroupProject("PYTHON_TEST_PROJECT", members, "PYTHON_TEST_CLASS", "clunke", "C:\aws\keys\PYTHON_TEST_PROJECT.pub")
+	
+	#Run the group project
 	group_project.run()
